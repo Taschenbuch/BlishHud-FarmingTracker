d360b4aabc28859f682523223ed9ddf1b636e174
Taschenbuch <43114787+Taschenbuch@users.noreply.github.com>
2024-05-18 21:56:26 +0200

    fix texture type

--- FarmingTracker/Controls/ClickThroughImage.cs
+++ FarmingTracker/Controls/ClickThroughImage.cs
@@ -1,12 +1,13 @@
 ﻿using Blish_HUD.Content;
 using Blish_HUD.Controls;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace FarmingTracker
 {
     public class ClickThroughImage : Image
     {
-        public ClickThroughImage(AsyncTexture2D texture, Point location, Container parent) : base(texture)
+        public ClickThroughImage(Texture2D texture, Point location, Container parent) : base(texture)
         {
             Location = location;
             Parent = parent;
