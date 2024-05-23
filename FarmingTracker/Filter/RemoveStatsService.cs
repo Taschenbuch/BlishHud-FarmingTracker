7d53d932e15400d68c7efec1cc78fbe2bb97b991
Taschenbuch <43114787+Taschenbuch@users.noreply.github.com>
2024-05-23 22:03:17 +0200

    refactor: extract RemoveStatsService

---
+++ FarmingTracker/Filter/RemoveStatsService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FarmingTracker
+{
+    public class RemoveStatsService
+    {
+        public static (List<Stat> items, List<Stat> currencies) RemoveStatsNotUpdatedYetDueToApiError(List<Stat> items, List<Stat> currencies)
+        {
+            items = RemoveStatsNotUpdatedYetDueToApiError(items);
+            currencies = RemoveStatsNotUpdatedYetDueToApiError(currencies);
+
+            return (items, currencies);
+        }
+
+        // normally after an api error, the UI is not updated. So stats that did not get api details yet, do not show up in the UI until the next success api call.
+        // But when the UI is updated due to a user action (changed sort, changed filter, ...), those missing-details-stats would be displayed without name, icon, tooltip.
+        // this method prevents that they are displayed.
+        private static List<Stat> RemoveStatsNotUpdatedYetDueToApiError(List<Stat> stats)
+        {
+            return stats
+                .Where(c => c.Details.State != ApiStatDetailsState.MissingBecauseApiNotCalledYet)
+                .ToList();
+        }
+    }
+}
--- FarmingTracker/SessionSummary/UiUpdater.cs
+++ FarmingTracker/SessionSummary/UiUpdater.cs
@@ -11,7 +11,7 @@ namespace FarmingTracker
             var currencies = services.Stats.CurrencyById.Values.Where(c => !c.IsCoin).ToList(); // remove coin before the counting-to-check-if-stats-were-filtered
             var items = services.Stats.ItemById.Values.ToList();
 
-            (items, currencies) = RemoveStatsNotUpdatedYetDueToApiError(items, currencies, services);
+            (items, currencies) = RemoveStatsService.RemoveStatsNotUpdatedYetDueToApiError(items, currencies);
             (items, currencies) = SearchService.FilterBySearchTerm(items, currencies, services.SearchTerm);
             (items, currencies) = FilterService.FilterStatsAndSetFunnelOpacity(items, currencies, statsPanels, services);
             (items, currencies) = SortService.SortStats(items, currencies, services);
@@ -30,24 +30,6 @@ namespace FarmingTracker
 
             if (statsPanels.FarmedCurrenciesFlowPanel.IsEmpty())
                 new HintLabel(statsPanels.FarmedCurrenciesFlowPanel, $"{PADDING}No currency changes detected!");
-        }
-
-        private static (List<Stat> items, List<Stat> currencies) RemoveStatsNotUpdatedYetDueToApiError(List<Stat> items, List<Stat> currencies, Services services)
-        {
-            items = RemoveStatsNotUpdatedYetDueToApiError(items);
-            currencies = RemoveStatsNotUpdatedYetDueToApiError(currencies);
-
-            return (items, currencies);
-        }
-
-        // normally after an api error, the UI is not updated. So stats that did not get api details yet, do not show up in the UI until the next success api call.
-        // But when the UI is updated due to a user action (changed sort, changed filter, ...), those missing-details-stats would be displayed without name, icon, tooltip.
-        // this method prevents that they are displayed.
-        private static List<Stat> RemoveStatsNotUpdatedYetDueToApiError(List<Stat> stats)
-        {
-            return stats
-                .Where(c => c.Details.State != ApiStatDetailsState.MissingBecauseApiNotCalledYet)
-                .ToList();
         }
 
         private static ControlCollection<Control> CreateStatControls(List<Stat> stats, Services services)
