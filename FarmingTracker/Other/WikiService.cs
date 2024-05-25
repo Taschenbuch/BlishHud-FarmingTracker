eff12374af28e64ce40059be8c6a62ed61b58121
Taschenbuch <43114787+Taschenbuch@users.noreply.github.com>
2024-05-25 20:21:46 +0200

    fix: currency could not be found in wiki because of localized name
    * items do not have this issue because they use chat codes. Those are language independent

--- FarmingTracker/Other/WikiService.cs
+++ FarmingTracker/Other/WikiService.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
 using System;
+using Blish_HUD;
+using Gw2Sharp.WebApi;
 
 namespace FarmingTracker
 {
@@ -7,7 +9,9 @@ namespace FarmingTracker
     {
         public static void OpenWikiSearchInDefaultBrowser(string wikiSearchTerm)
         {
-            var url = $"https://wiki.guildwars2.com/index.php?&search={Uri.EscapeDataString(wikiSearchTerm)}";
+            var languageString = GetWikiLanguageString();
+            var percentEscapedSearchTerm = Uri.EscapeDataString(wikiSearchTerm);
+            var url = $"https://wiki-{languageString}.guildwars2.com/index.php?&search={percentEscapedSearchTerm}";
             OpenUrlInDefaultBrowser(url);
         }
 
@@ -15,6 +19,27 @@ namespace FarmingTracker
         {
             var url = $"https://wiki.guildwars2.com/wiki/Special:RunQuery/Search_by_id?title=Special%3ARunQuery%2FSearch_by_id&pfRunQueryFormName=Search+by+id&Search+by+id%5Bid%5D={apiId}&Search+by+id%5Bcontext%5D=&wpRunQuery=&pf_free_text=";
             OpenUrlInDefaultBrowser(url);
+        }
+
+        private static string GetWikiLanguageString()
+        {
+            switch (GameService.Overlay.UserLocale.Value)
+            {
+                case Locale.Spanish:
+                    return "es";
+                case Locale.German:
+                    return "de";
+                case Locale.French:
+                    return "fr";
+                // Korean and chinese do not their own wikis.
+                // The wiki search will work for the items chat codes. Because those are language independent.
+                // It wont work for the localized names of currencies.
+                case Locale.Korean: 
+                case Locale.Chinese:
+                case Locale.English:
+                default:
+                    return "en";
+            }
         }
 
         private static void OpenUrlInDefaultBrowser(string url)
