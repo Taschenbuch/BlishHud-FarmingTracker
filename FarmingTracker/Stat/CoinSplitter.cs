902b5bc25c19880ee1d9e2383c3668e09d3894e7
Taschenbuch <43114787+Taschenbuch@users.noreply.github.com>
2024-05-25 19:49:07 +0200

    allow right click to open wiki for raw gold

--- FarmingTracker/Stat/CoinSplitter.cs
+++ FarmingTracker/Stat/CoinSplitter.cs
@@ -12,23 +12,24 @@ namespace FarmingTracker
             if (noCoinsEarnedOrLost)
                 return currencies;
 
+            var localizedCoinName = coinStat.Details.Name; // for wiki because "coin" in spanish will not work in english wiki
             var coin = new Coin(coinStat.Count);
 
             if(coin.HasToDisplayGold)
             {
-                var goldStat = CreateCoinStat("Gold", coin.Gold, GOLD_FAKE_API_ID, ApiStatDetailsState.GoldCoinCustomStat);
+                var goldStat = CreateCoinStat("Gold", coin.Gold, GOLD_FAKE_API_ID, ApiStatDetailsState.GoldCoinCustomStat, localizedCoinName);
                 currencies.Add(goldStat);
             }
 
             if(coin.HasToDisplaySilver) 
             {
-                var silverStat = CreateCoinStat("Silver", coin.Silver, SILVER_FAKE_API_ID, ApiStatDetailsState.SilveCoinCustomStat);
+                var silverStat = CreateCoinStat("Silver", coin.Silver, SILVER_FAKE_API_ID, ApiStatDetailsState.SilveCoinCustomStat, localizedCoinName);
                 currencies.Add(silverStat);
             }
             
             if(coin.HasToDisplayCopper)
             {
-                var copperStat = CreateCoinStat("Copper", coin.Copper, COPPER_FAKE_API_ID, ApiStatDetailsState.CopperCoinCustomStat);
+                var copperStat = CreateCoinStat("Copper", coin.Copper, COPPER_FAKE_API_ID, ApiStatDetailsState.CopperCoinCustomStat, localizedCoinName);
                 currencies.Add(copperStat);
             }
             
@@ -36,7 +37,7 @@ namespace FarmingTracker
             return currencies;
         }
 
-        private static Stat CreateCoinStat(string name, long count, int apiId, ApiStatDetailsState apiStatDetailsState)
+        private static Stat CreateCoinStat(string name, long count, int apiId, ApiStatDetailsState apiStatDetailsState, string localizedCoinName)
         {
             return new Stat
             {
@@ -45,6 +46,7 @@ namespace FarmingTracker
                 Details =
                 {
                     Name = name,
+                    WikiSearchTerm = localizedCoinName,
                     State = apiStatDetailsState // to get get correct coin texture later
                 },
             };
