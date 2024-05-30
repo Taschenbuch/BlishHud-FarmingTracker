9384b9f1c34843316833e0cc2f7feb2581fe0eda
Taschenbuch <43114787+Taschenbuch@users.noreply.github.com>
2024-05-30 14:21:58 +0200

    move negative profit sign closer to coin value

--- FarmingTracker/Profit/ProfitPanel.cs
+++ FarmingTracker/Profit/ProfitPanel.cs
@@ -9,17 +9,19 @@ namespace FarmingTracker
         public ProfitPanel(string suffixText, string tooltip, BitmapFont font, Services services, Container parent, int height = 0)
         {
             FlowDirection = ControlFlowDirection.SingleLeftToRight;
-            ControlPadding = new Vector2(5, 0);
             BasicTooltipText = tooltip;
             WidthSizingMode = SizingMode.AutoSize;
-            Parent = parent;
 
             if(height > 0)
                 Height = height;
             else
                 HeightSizingMode = SizingMode.AutoSize;
 
-            _rootPanel = new FlowPanel
+            Parent = parent;
+
+            _signLabel = new CoinSignLabel(tooltip, font, this);
+
+            _coinsFlowPanel = new FlowPanel // this does not include the suffix Text Label
             {
                 FlowDirection = ControlFlowDirection.SingleLeftToRight,
                 ControlPadding = new Vector2(5, 0),
@@ -29,34 +31,35 @@ namespace FarmingTracker
                 Parent = this,
             };
 
-            _signLabel = new CoinSignLabel(tooltip, font, _rootPanel);
-            _goldPanel = new CoinPanel(services.TextureService.SmallGoldCoinTexture, Color.Gold, tooltip, font, false, _rootPanel);
-            _silverPanel = new CoinPanel(services.TextureService.SmallSilverCoinTexture, Color.LightGray, tooltip, font, true, _rootPanel);
-            _copperPanel = new CoinPanel(services.TextureService.SmallCopperCoinTexture, Color.SandyBrown, tooltip, font, true, _rootPanel);
+            _goldPanel = new CoinPanel(services.TextureService.SmallGoldCoinTexture, Color.Gold, tooltip, font, false, _coinsFlowPanel);
+            _silverPanel = new CoinPanel(services.TextureService.SmallSilverCoinTexture, Color.LightGray, tooltip, font, true, _coinsFlowPanel);
+            _copperPanel = new CoinPanel(services.TextureService.SmallCopperCoinTexture, Color.SandyBrown, tooltip, font, true, _coinsFlowPanel);
 
-            new Label
-            {
-                Text = suffixText,
-                Font = font,
-                BasicTooltipText = tooltip,
-                AutoSizeHeight = true,
-                AutoSizeWidth = true,
-                Parent = this,
-            };
+            var hasSuffixText = !string.IsNullOrWhiteSpace(suffixText);
+            if(hasSuffixText)
+                new Label
+                {
+                    Text = $" {suffixText}", // add padding here instead of using flowPanel.Controlpadidng because sign label must not have padding.
+                    Font = font,
+                    BasicTooltipText = tooltip,
+                    AutoSizeHeight = true,
+                    AutoSizeWidth = true,
+                    Parent = this,
+                };
         }
 
         public void SetProfit(long profitInCopper)
         {
             var coin = new Coin(profitInCopper);
 
-            _signLabel.SetSign(coin.Sign); // always show sign label
+            _signLabel.SetSign(coin.Sign); // always show sign label to prevent moving it to the end accidently
             // order of setting gold, silver, copper is important because parent is flowpanel!
             _goldPanel.SetValue(coin.UnsignedGold, false);
             _silverPanel.SetValue(coin.UnsignedSilver, coin.UnsignedGold != 0);
             _copperPanel.SetValue(coin.UnsignedCopper, true); // always show copper
         }
 
-        private readonly FlowPanel _rootPanel;
+        private readonly FlowPanel _coinsFlowPanel;
         private readonly CoinSignLabel _signLabel;
         private readonly CoinPanel _goldPanel;
         private readonly CoinPanel _silverPanel;
