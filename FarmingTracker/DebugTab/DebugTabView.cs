b868f9d6cba6c210448f90bcb2c27ecc82251d84
Taschenbuch <43114787+Taschenbuch@users.noreply.github.com>
2024-05-31 23:16:38 +0200

    fix: DebugTabView was not committed because folder was called "Debug"

---
+++ FarmingTracker/DebugTab/DebugTabView.cs
@@ -0,0 +1,20 @@
+﻿using Blish_HUD.Controls;
+using Blish_HUD.Graphics.UI;
+
+namespace FarmingTracker
+{
+    public class DebugTabView : View
+    {
+        public DebugTabView(Services services)
+        {
+            _services = services;
+        }
+
+        protected override void Build(Container buildPanel)
+        {
+            SettingControls.CreateSetting(buildPanel, _services.SettingService.IsFakeDrfServerUsedSetting);
+        }
+
+        private readonly Services _services;
+    }
+}
