1e179b5a1277b61785fc7611f9cb30da1a3ca9be
Taschenbuch <43114787+Taschenbuch@users.noreply.github.com>
2024-09-09 17:33:56 +0200

    remove redundant using

--- FarmingTracker/Profit/ProfitPanels.cs
+++ FarmingTracker/Profit/ProfitPanels.cs
@@ -3,7 +3,6 @@ using Blish_HUD.Controls;
 using MonoGame.Extended.BitmapFonts;
 using System;
 using System.Diagnostics;
-using System.Linq;
 using static Blish_HUD.ContentService;
 
 namespace FarmingTracker
