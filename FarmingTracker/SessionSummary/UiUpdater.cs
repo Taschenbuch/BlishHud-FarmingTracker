3337102ef3b11b5a8e79d06e9ea76c2655c781a6
Taschenbuch <43114787+Taschenbuch@users.noreply.github.com>
2024-09-22 21:51:04 +0200

    fix: dont show new tracked favorite items as bug icons when gw2 api fails. hide them instead like regular items and currencies.

--- FarmingTracker/SessionSummary/UiUpdater.cs
+++ FarmingTracker/SessionSummary/UiUpdater.cs
@@ -11,11 +11,11 @@ namespace FarmingTracker
 
             var (items, currencies) = StatsService.ShallowCopyStatsToPreventModification(snapshot);
             (items, currencies) = StatsService.RemoveZeroCountStats(items, currencies); // dont call this AFTER the coin splitter. it would remove them.
+            (items, currencies) = StatsService.RemoveStatsNotUpdatedYetDueToApiError(items, currencies);
             List<Stat> favoriteItems;
             (favoriteItems, items) = StatsService.SplitIntoFavoriteAndRegularItems(items, favoriteItemApiIds);
             items = StatsService.RemoveIgnoredItems(items, model.IgnoredItemApiIds.ToListSafe());
             currencies = CoinSplitter.ReplaceCoinWithGoldSilverCopperStats(currencies);
-            (items, currencies) = StatsService.RemoveStatsNotUpdatedYetDueToApiError(items, currencies);
             (items, currencies) = SearchService.FilterBySearchTerm(items, currencies, services.SearchTerm);
             (items, currencies) = FilterService.FilterStatsAndSetFunnelOpacity(items, currencies, statsPanels, services.SettingService);
             (items, currencies) = SortService.SortStats(items, currencies, services.SettingService);
