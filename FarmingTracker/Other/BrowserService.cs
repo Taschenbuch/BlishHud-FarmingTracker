c43c3c61f740973a5437a0ca09236bb319bcbcb5
Taschenbuch <43114787+Taschenbuch@users.noreply.github.com>
2025-01-04 19:18:48 +0100

    refactor: extract browserService

---
+++ FarmingTracker/Other/BrowserService.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System;
+
+namespace FarmingTracker
+{
+    public class BrowserService
+    {
+        public static void OpenUrlInDefaultBrowser(string url)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception e)
+            {
+                Module.Logger.Error(e, "Failed to open url in default browser.");
+            }
+        }
+    }
+}
--- FarmingTracker/Other/WikiService.cs
+++ FarmingTracker/Other/WikiService.cs
@@ -12,13 +12,13 @@ namespace FarmingTracker
             var languageString = GetWikiLanguageString();
             var percentEscapedSearchTerm = Uri.EscapeDataString(wikiSearchTerm);
             var url = $"https://wiki-{languageString}.guildwars2.com/index.php?&search={percentEscapedSearchTerm}";
-            OpenUrlInDefaultBrowser(url);
+            BrowserService.OpenUrlInDefaultBrowser(url);
         }
 
         public static void OpenWikiIdQueryInDefaultBrowser(int apiId)
         {
             var url = $"https://wiki.guildwars2.com/wiki/Special:RunQuery/Search_by_id?title=Special%3ARunQuery%2FSearch_by_id&pfRunQueryFormName=Search+by+id&Search+by+id%5Bid%5D={apiId}&Search+by+id%5Bcontext%5D=&wpRunQuery=&pf_free_text=";
-            OpenUrlInDefaultBrowser(url);
+            BrowserService.OpenUrlInDefaultBrowser(url);
         }
 
         // Korean and chinese do not have their own wikis.
@@ -33,22 +33,6 @@ namespace FarmingTracker
                 Locale.French => "fr",
                 _ => "en",
             };
-        }
-
-        private static void OpenUrlInDefaultBrowser(string url)
-        {
-            try
-            {
-                Process.Start(new ProcessStartInfo
-                {
-                    FileName = url,
-                    UseShellExecute = true
-                });
-            }
-            catch (Exception e)
-            {
-                Module.Logger.Error(e, "Failed to open url in default browser.");
-            }
         }
     }
 }
